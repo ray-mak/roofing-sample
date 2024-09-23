bf41f540d6388e3d569edc33568877a596226f84
Ray Mak <projectraymak@gmail.com>
2024-09-22 20:08:54 -0700

    Create Navbar

---
+++ src/components/HamburgerMenu.tsx
@@ -0,0 +1,80 @@
+"use client"
+import { Oswald } from "next/font/google"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { useState } from "react"
+
+const oswald = Oswald({
+  subsets: ["latin"],
+  weight: ["400"],
+  display: "swap",
+})
+
+export default function HamburgerMenu() {
+  const pathname = usePathname()
+  const [hamburgerMenu, setHamburgerMenu] = useState(false)
+  function toggleHamburger() {
+    setHamburgerMenu((prevState) => !prevState)
+  }
+
+  function closeHamburger() {
+    setHamburgerMenu(false)
+  }
+  return (
+    <>
+      <div
+        onClick={toggleHamburger}
+        className={`hamburger  ${hamburgerMenu ? "opened" : ""}`}
+      >
+        <span className="bar bg-gray-200"></span>
+        <span className="bar bg-gray-200"></span>
+        <span className="bar bg-gray-200"></span>
+      </div>
+      <div className={`dimmer ${hamburgerMenu ? "opened" : ""}`}></div>
+      <div
+        className={`navmenu ${
+          oswald.className
+        } uppercase bg-gray-800 md:bg-inherit flex gap-6 ml-auto items-center justify-center text-2xl text-gray-100 ${
+          hamburgerMenu ? "opened" : ""
+        } `}
+      >
+        <Link
+          href="/"
+          className={`underline-animation font-medium md:underline-offset-[25px] decoration-4 decoration-amber-700 hover:text-amber-700 ${
+            pathname === "/" ? "underline text-amber-700" : ""
+          }`}
+          onClick={closeHamburger}
+        >
+          Home
+        </Link>
+        <Link
+          href="/services"
+          className={`underline-animation font-medium md:underline-offset-[25px] decoration-4 decoration-amber-700  hover:text-amber-700 ${
+            pathname === "/services" ? "underline text-amber-700" : ""
+          }`}
+          onClick={closeHamburger}
+        >
+          Services
+        </Link>
+        <Link
+          href="/about"
+          className={`underline-animation font-medium md:underline-offset-[25px] decoration-4 decoration-amber-700 hover:text-amber-700 ${
+            pathname === "/about" ? "underline text-amber-700" : ""
+          }`}
+          onClick={closeHamburger}
+        >
+          About
+        </Link>
+        <Link
+          href="/contact"
+          className={`underline-animation font-medium md:underline-offset-[25px] decoration-4 decoration-amber-700 hover:text-amber-700 ${
+            pathname === "/contact" ? "underline text-amber-700" : ""
+          }`}
+          onClick={closeHamburger}
+        >
+          Contact
+        </Link>
+      </div>
+    </>
+  )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+"use client"
+import Image from "next/image"
+import Link from "next/link"
+import HamburgerMenu from "./HamburgerMenu"
+
+export function Navbar() {
+  return (
+    <nav className="navbar border-b border-gray-600 fixed top-0 z-20 flex w-full h-[80px] p-4 md:p-0 flex items-center justify-center bg-gray-900 shadow-xl">
+      <div className="w-full md:w-5/6 lg:w-3/4 lg:max-w-6xl flex items-center ">
+        <Link href="/" className={`text-4xl font-bold `}>
+          <Image
+            src="/images/logolight.svg"
+            width={180}
+            height={60}
+            alt="codecraft web design logo"
+          />
+        </Link>
+        <HamburgerMenu />
+      </div>
+    </nav>
+  )
+}
